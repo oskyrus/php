54a09e258173012ac08be859f424be4348f188d1
oskyrus <76231935+oskyrus@users.noreply.github.com>
2026-04-15 12:06:34 +0200

    Add files via upload

---
+++ phptest/process.php
@@ -0,0 +1,15 @@
+<?php
+
+$imie = $_POST['imie'];
+$wiek = $_POST['wiek'];
+$plec = $_POST['plec'];
+$zgoda = $_POST['zgoda'];
+
+echo $imie;
+echo $wiek;
+echo $plec;
+echo $zgoda;
+
+
+echo "<h2> $imie </h2> <p> $wiek  </p> <h1> $plec </h1> <h3> $zgoda </h3>";
+
