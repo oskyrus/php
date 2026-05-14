70cb6b0328c0c052912a5817cbe2e7c590dbc4c0
oskyrus <76231935+oskyrus@users.noreply.github.com>
2026-05-14 10:26:14 +0200

    Add files via upload

--- zadania2/index.php
+++ zadania2/index.php
@@ -1,80 +1,188 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zadania php https://github.com/TEB-DK/Domena_aplikacji_internetowych/blob/J%C4%99zyk-PHP/1.%20Podstawowa%20sk%C5%82adnia.md </title>
+    <title>Zadania php
+        https://github.com/TEB-DK/Domena_aplikacji_internetowych/blob/J%C4%99zyk-PHP/1.%20Podstawowa%20sk%C5%82adnia.md
+    </title>
     <style>
         body {
             display: flex;
-            flex-direction:column;
+            flex-direction: column;
             margin: 100px 20px;
+            font-family: 'Trebuchet MS', sans-serif;
         }
-        .wraper{
+
+        .wraper {
             margin: 0px 100px;
 
         }
-        h1{
+
+        h1 {
             margin: 0;
         }
+
         .divv {
             display: flex;
             margin: 0px;
         }
+
         p {
             margin: 25px 2px;
         }
-</style>
+    </style>
 </head>
+
 <body>
     <div class="wraper">
-    <h1>Zadania Tablice</h1>
-    <?php
-    $fruits = ["jabłko", "banan", "gruszka"];
-    $colors = ['red','blue','green'];
-    $numbers = [1, 2, 3, 4];
-    $user = [
-        'name' => 'Jan',
-        'age' => 28,
-        'city' => 'Warszawa'
-    ];
-    
-    echo "<h2>1.$colors[0]</h2> ";
-    
-    $product = [
+        <h1>1. Zadania Tablice</h1>
+        <?php
+        $fruits = ["jabłko", "banan", "gruszka"];
+        $colors = ['red', 'blue', 'green'];
+        $numbers = [1, 2, 3, 4];
+        $user = [
+            'name' => 'Jan',
+            'age' => 28,
+            'city' => 'Warszawa'
+        ];
+        echo "<h2>1.$colors[0]</h2> ";
+        echo "<div class='divv'><h2>2.    $ product = [
         'name' => 'Jan',
         'price' => 28,
         'in_stock' => true
     ];
-    
-    
-    array_push($numbers, '5');
-    
-    
-    echo "<div class='divv'><h2>3. Nowy Element </h2><p>$numbers[4]</p></div>";
-    
-    if (in_array("banan", $fruits))
-    {
-        echo "<div class='divv'><h2>4. znaleziono ' banan ' w tablicy fruits</h2></div>";
-    }
-    else
-    {
-        echo "4. nie znaleziono";
-    }
-    
-    ?>
-    <div class="divv">
-    <h2>
-    <?php
-        echo "5. ";
-        $keys = array_keys($user);
-        print_r($keys);
-    ?>
-    </h2>
+    </h2></div>";
+        $product = [
+            'name' => 'Jan',
+            'price' => 28,
+            'in_stock' => true
+        ];
+
+
+        array_push($numbers, '5');
+
+
+        echo "<div class='divv'><h2>3. Nowy Element </h2><p>$numbers[4]</p></div>";
+
+        if (in_array("banan", $fruits)) {
+            echo "<div class='divv'><h2>4. znaleziono ' banan ' w tablicy fruits</h2></div>";
+        } else {
+            echo "4. nie znaleziono";
+        }
+
+        ?>
+        <div class="divv">
+            <h2>
+                <?php
+                echo "5. ";
+                $keys = array_keys($user);
+                print_r($keys);
+                ?>
+            </h2>
+        </div>
+        <h1>2. Zadania Funkcje</h1>
+        <div class="divv">
+
+            <?php
+            function sayHello()
+            {
+                print_r("Witaj!");
+            }
+            echo "<h2>1. </h2>";
+            sayHello();
+            ?>
+        </div>
+        <div class="divv">
+
+            <?php
+            function multiply($a, $b)
+            {
+                return $a * $b;
+
+            }
+            $wynik = multiply(5, 20);
+            echo "<h2>2. </h2>";
+            echo $wynik;
+            ?>
+        </div>
+        <div class="divv">
+
+            <?php
+            function isEven($n)
+            {
+                if ($n & 2) {
+                    echo 'parzysta';
+                } else {
+                    echo 'nieparzysta';
+                }
+            }
+            echo "<h2>3. </h2>";
+            isEven(5);
+            ?>
+        </div>
+        <div class="divv">
+
+            <?php
+
+            function formatPrice($price)
+            {
+                $price = number_format($price, 2, ".", "");
+                return $price;
+            }
+            echo "<h2>4. </h2>";
+            echo formatPrice(5);
+            ?>
+        </div>
+        <div class="divv">
+
+            <?php
+            echo "<h2>5. </h2>";
+            function greetUser($name, $age)
+            {
+                echo "Witaj $name lat $age";
+
+            }
+            greetUser("Marcin", "14");
+            ?>
+        </div>
+        <h1 id="z3">3. Zadania Ciasteczka</h1>
+        <div class="divv">
+            <h2>1. Ustawiono ciasteczka imienia </h2>
+            <?php
+
+            $siedemdni = 86400 * 7;
+            setcookie("username", "Jan", $siedemdni, "/");
+            ?>
+
+        </div>
+        <div class="divv">
+            <h2>2. </h2>
+            <?php
+
+
+            if (isset($_COOKIE["username"])) {
+                echo "Witaj, " . $_COOKIE["username"];
+            } else {
+                echo "cześć bezimienny";
+            }
+
+            ?>
+
+        </div>
+        <div class="divv">
+            <?php
+
+            setcookie("background_color", "black", $siedemdni, "/");
+            ?>
+            <h2>3. Ustawiono ciasteczka koloru</h2>
+
         </div>
     </div>
-    
-    
 
+
+    </div>
 </body>
+
 </html>
